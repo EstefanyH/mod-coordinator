9ee6f1ff4c368ca7f113ba61e3778153fb3a828c
EstefanyH <estefanyhuamani287@gmail.com>
2024-06-12 15:51:55 -0500

    Create proyect package coordinator

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Coordinator-Module",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Coordinator-Module",
+            targets: ["Coordinator-Module"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Coordinator-Module"),
+        .testTarget(
+            name: "Coordinator-ModuleTests",
+            dependencies: ["Coordinator-Module"]),
+    ]
+)
